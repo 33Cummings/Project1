fb23eda6fab414d66aee4002d3e1a133a23bd2b7
33Cummings <44670614+33Cummings@users.noreply.github.com>
2018-11-01 07:56:59 -0400

    Add files via upload

--- Personality Quiz CM.py
+++ Personality Quiz CM.py
@@ -1,52 +1,57 @@
 import webbrowser as wb
 import pyautogui as pg
+import time as t
 
-name = input("What's your name? ")
+name = pg.prompt("What's your name? ")
 if name == "Campbell":
-    print("What a pretty name!")
+    pg.alert("What a pretty name!")
 elif name == "campbell":
-    print("What a pretty name!")
+    pg.alert("What a pretty name!")
 elif name == "Julia":
-    print("What a pretty name!")
+    pg.alert("What a pretty name!")
 elif name == "julia":
-    print("What a pretty name!")
+    pg.alert("What a pretty name!")
 else:
-    print("You're my new best friend " + name + "!")
+    pg.alert("You're my new best friend " + name + "!")
 
-fruit = input("What is your favorite fruit? ")
+fruit = pg.prompt("What is your favorite fruit? ")
 if fruit == "watermelon":
-    print("Refreshing!")
+    pg.alert("Refreshing!")
 elif fruit == "Watermelon":
-    print("Refreshing")
+    pg.alert("Refreshing")
 elif fruit == "apple":
-    print("An apple a day keeps the doctor away!")
+    pg.alert("An apple a day keeps the doctor away!")
 elif fruit == "Apple":
-    print("An apple a day keeps the doctor away!")
+    pg.alert("An apple a day keeps the doctor away!")
 elif fruit == "strawberry":
-    print("Strawberrylicious!")
+    pg.alert("Strawberrylicious!")
 elif fruit == "Strawberry":
-    print("Strawberrylicious!")
+    pg.alert("Strawberrylicious!")
+elif fruit == "strawberries":
+    pg.alert("Strawberrylicious!")
+elif fruit == "Strawberries":
+    pg.alert("Strawberrylicious!")
 else:
-    print("Yum! I love " + fruit + "s" )
+    pg.alert("Yum! I love " + fruit + "s" )
     wb.open("")
 
-boardgame = input("What's your favorite boardgame? ")
+boardgame = pg.prompt("What's your favorite boardgame? ")
 if boardgame == "monopoly":
-    print("I'm a beast at monopoly")
+    pg.alert("I'm a beast at monopoly")
 elif boardgame == "Monopoly":
-    print("I'm a beast at monopoly")
+    pg.alert("I'm a beast at monopoly")
 elif boardgame == "life":
-    print("Do you like the cereal too?")
+    pg.alert("Do you like the cereal too?")
 elif boardgame == "Life":
-    print("Do you like the cereal too?")
+    pg.alert("Do you like the cereal too?")
 else:
-    print("Cool beans! I love " + boardgame + "!!")
+    pg.alert("Cool beans! I love " + boardgame + "!!")
     wb.open("")
 
-birthday = input("What month is your birthday? ")
+birthday = pg.prompt("What month is your birthday? ")
 wb.open("https://www.google.com/search?q=" + birthday + " birthstone")
 
-pup_kit = input("Do you like puppies or kittens better? ")
+pup_kit = pg.prompt("Do you like puppies or kittens better? ")
 if pup_kit == "puppies":
     wb.open("https://www.youtube.com/watch?v=p079QtWvf7o")
 elif pup_kit == "Puppies":
@@ -55,3 +60,8 @@ elif pup_kit == "kittens":
     wb.open("https://www.youtube.com/watch?v=8HVWitAW-Qg")
 elif pup_kit == "Kittens":
     wb.open("https://www.youtube.com/watch?v=8HVWitAW-Qg")
+
+age = pg.prompt("What year were you born?")
+pg.alert("You are "  + str(2018 - int(age)) + " years old!")
+
+
